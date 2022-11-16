4924bb652a38dbbc551d5b2dfce51617cd828538
pallabmondal185 <pallabmondal185@gmail.com>
2022-11-16 19:48:39 +0530

    hello pallab

--- src/addingGit/pallab.jsx
+++ src/addingGit/pallab.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const pallab = () => {
   return (
     <>
-      <h1>hello pallab44444444444444444444444444444444444444444444444444444444444444444444444444</h1>
+      <h1>hello pallab</h1>
+      <h3>hello world</h3>
     </>
   )
 }
